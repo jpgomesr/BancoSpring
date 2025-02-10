40c2ce74e3ecfae095173c1a6ccc83f4b41a9f48
joao_pg_rodrigues <joao_pg_rodrigues@estudante.sesisenai.org.br>
2025-02-10 16:28:14 -0300

    feat: Criação dos repositorys

---
+++ src/main/java/net/weg/mi75/repository/ContaRepository.java
@@ -0,0 +1,9 @@
+package net.weg.mi75.repository;
+
+import net.weg.mi75.models.Conta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContaRepository extends JpaRepository<Conta, String> {
+}
