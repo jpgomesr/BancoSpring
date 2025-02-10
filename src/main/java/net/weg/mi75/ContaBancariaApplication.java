757a5f85d09e113ece2c4c0af358a09a2ed19165
joao_pg_rodrigues <joao_pg_rodrigues@estudante.sesisenai.org.br>
2025-02-10 16:28:30 -0300

    feat: App & Test

---
+++ src/main/java/net/weg/mi75/ContaBancariaApplication.java
@@ -0,0 +1,13 @@
+package net.weg.mi75;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaBancariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaBancariaApplication.class, args);
+	}
+
+}
