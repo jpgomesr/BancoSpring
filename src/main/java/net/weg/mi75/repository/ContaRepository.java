36455f6eac71440d65faf533b35bc34c4a8ef93e
João Paulo Gomes Rodrigues <joao_pg_rodrigues@estudante.sesisenai.org.br>
2025-02-19 15:30:11 -0300

    feat: Repository de tudo

--- src/main/java/net/weg/mi75/repository/ContaRepository.java
+++ src/main/java/net/weg/mi75/repository/ContaRepository.java
@@ -1,9 +1,9 @@
 package net.weg.mi75.repository;
 
-import net.weg.mi75.models.Conta;
+import net.weg.mi75.models.entity.Conta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ContaRepository extends JpaRepository<Conta, String> {
+public interface ContaRepository extends JpaRepository<Conta, Integer> {
 }
