d6b7e06030a8a406e7aad80abcab5f11973b14b2
João Paulo Gomes Rodrigues <joao_pg_rodrigues@estudante.sesisenai.org.br>
2025-02-20 13:21:45 -0300

    feat: criado repository de transferencia

--- src/main/java/net/weg/mi75/repository/TransferenciaRespository.java
+++ src/main/java/net/weg/mi75/repository/TransferenciaRespository.java
@@ -1,9 +1,9 @@
-//package net.weg.mi75.repository;
-//
-//import net.weg.mi75.models.entity.Transferencia;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface TransferenciaRespository extends JpaRepository<Transferencia, String> {
-//}
+package net.weg.mi75.repository;
+
+import net.weg.mi75.models.entity.Transferencia;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransferenciaRespository extends JpaRepository<Transferencia, String> {
+}
